18cbc4b8cf83191ba42501014cbb47b51cc00549
JangJuMan <wnaks9901@gmail.com>
2025-09-08 15:49:15 +0900

    [Git Init] Git initialization
    1. 에셋세팅 (닭, 달걀, 레벨디자인용 에셋 등)
    2. 폴더구조 세팅
    3. EGLog 파일 생성

---
+++ Source/EG/EGLog.cpp
@@ -0,0 +1,14 @@
+// EGLog.cpp
+
+#include "EGLog.h"
+#include "Modules/ModuleManager.h"
+
+
+/* DedicatedX Log 예시 */
+/*
+#pragma region NetLogging
+
+DEFINE_LOG_CATEGORY(LogDXNet);
+
+#pragma endregion
+*/
