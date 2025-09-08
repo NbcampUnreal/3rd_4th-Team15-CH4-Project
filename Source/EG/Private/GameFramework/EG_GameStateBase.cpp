787d617ccd0826ee24b3033eb8b413826edc3dbf
BannedUser0 <vvmyu13@gmail.com>
2025-09-08 16:37:09 +0900

    **Add EG_GameStateBase and EG_GameModeBase classes**

---
+++ Source/EG/Private/GameFramework/EG_GameStateBase.cpp
@@ -0,0 +1,4 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EG/Public/EG_GameStateBase.h"
---
+++ Source/EG/Public/GameFreamework/EG_GameStateBase.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "EG_GameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EG_API AEG_GameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+};
