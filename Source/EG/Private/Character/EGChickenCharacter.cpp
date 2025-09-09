878932f326fef23955a2fad71210b3540a898cf1
JangJuMan <wnaks9901@gmail.com>
2025-09-09 14:27:46 +0900

    [Add] : ChickenCharacter 프로토타입 제작
    기본적인 Move, Look, Jump, Dash, Sprint 기능 포함
    
    애니메이션 미포함(아직 에셋이 없음)

---
+++ Source/EG/Private/Character/EGChickenCharacter.cpp
@@ -0,0 +1,118 @@
+﻿// EGChickenCharacter.cpp (작성자 : JJM)
+
+#include "Public/Character/EGChickenCharacter.h"
+
+#include "EGLog.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+
+AEGChickenCharacter::AEGChickenCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bUseControllerDesiredRotation = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->TargetArmLength = 400.f;
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->bUsePawnControlRotation = false;
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+}
+
+void AEGChickenCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EIC = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::HandleMoveInput);
+
+	EIC->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::HandleLookInput);
+
+	EIC->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+	EIC->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+	EIC->BindAction(SprintAction, ETriggerEvent::Triggered, this, &ThisClass::HandleStartSprintInput);
+	EIC->BindAction(SprintAction, ETriggerEvent::Completed, this, &ThisClass::HandleStopSprintInput);
+	EIC->BindAction(DashAction, ETriggerEvent::Triggered, this, &ThisClass::HandleDash);
+}
+
+void AEGChickenCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (IsLocallyControlled() == true)
+	{
+		APlayerController* PC = Cast<APlayerController>(GetController());
+		checkf(IsValid(PC) == true, TEXT("PlayerController is invalid."));
+
+		UEnhancedInputLocalPlayerSubsystem* EILPS = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+		checkf(IsValid(EILPS) == true, TEXT("EnhancedInputLocalPlayerSubsystem is invalid."));
+
+		EILPS->AddMappingContext(InputMappingContext, 0);
+	}
+}
+
+void AEGChickenCharacter::HandleMoveInput(const FInputActionValue& InValue)
+{
+	if (IsValid(Controller) == false)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Controller is invalid."));
+		return;
+	}
+
+	const FVector2D InMovementVector = InValue.Get<FVector2D>();
+
+	const FRotator ControlRotation = Controller->GetControlRotation();
+	const FRotator ControlYawRotation(0.0f, ControlRotation.Yaw, 0.0f);
+
+	const FVector ForwardDirection = FRotationMatrix(ControlYawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(ControlYawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDirection, InMovementVector.X);
+	AddMovementInput(RightDirection, InMovementVector.Y);
+}
+
+void AEGChickenCharacter::HandleLookInput(const FInputActionValue& InValue)
+{
+	if (IsValid(Controller) == false)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Controller is invalid."));
+		return;
+	}
+
+	const FVector2D InLookVector = InValue.Get<FVector2D>();
+
+	AddControllerYawInput(InLookVector.X);
+	AddControllerPitchInput(InLookVector.Y);
+}
+
+void AEGChickenCharacter::HandleStartSprintInput()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 1000.f;
+}
+
+void AEGChickenCharacter::HandleStopSprintInput()
+{
+	
+	GetCharacterMovement()->MaxWalkSpeed = 600.f;
+}
+
+void AEGChickenCharacter::HandleDash()
+{
+	FVector Dir = GetActorForwardVector();
+	LaunchCharacter(Dir * 1000.f, true, true);
+	LOG_JM(LogJM, Warning, TEXT("Dashed"));
+}
---
+++ Source/EG/Public/Character/EGChickenCharacter.h
@@ -0,0 +1,66 @@
+﻿// EGChickenCharacter.h (작성자 : JJM)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "EGChickenCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class EG_API AEGChickenCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEGChickenCharacter();
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	virtual void BeginPlay() override;
+
+public:
+	FORCEINLINE USpringArmComponent* GetSpringArm() const { return SpringArm; }
+
+	FORCEINLINE UCameraComponent* GetCamera() const { return Camera; }
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AEGChickenCharacter|Components")
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AEGChickenCharacter|Components")
+	TObjectPtr<UCameraComponent> Camera;
+
+private:
+	void HandleMoveInput(const FInputActionValue& InValue);
+
+	void HandleLookInput(const FInputActionValue& InValue);
+	void HandleStartSprintInput();
+	void HandleStopSprintInput();
+	void HandleDash();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AEGChickenCharacter|Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AEGChickenCharacter|Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AEGChickenCharacter|Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AEGChickenCharacter|Input")
+	TObjectPtr<UInputAction> JumpAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AEGChickenCharacter|Input")
+	TObjectPtr<UInputAction> SprintAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AEGChickenCharacter|Input")
+	TObjectPtr<UInputAction> DashAction;
+
+};
