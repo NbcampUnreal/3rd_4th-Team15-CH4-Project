ce6d43c603694eae02ac25a81a12598fb6ee0295
khy1564 <comkhy@gmail.com>
2025-09-09 21:37:03 +0900

    feat: 게임 상태 추가
    - EG_GameStateBase에 AlivePlayerControllerCount, MatchState, RemainingCountdown, RemainingPlayTime 변수를 추가하고 리플리케이트합니다.

--- Source/EG/Private/GameFramework/EG_GameStateBase.cpp
+++ Source/EG/Private/GameFramework/EG_GameStateBase.cpp
@@ -1,4 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "EG/Public/EG_GameStateBase.h"
+#include "GameFreamework/EG_GameStateBase.h"
+
+#include "EG/EGLog.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+void AEG_GameStateBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, AlivePlayerControllerCount);
+	DOREPLIFETIME(ThisClass, MatchState);
+	DOREPLIFETIME(ThisClass, RemainingCountdown);
+	DOREPLIFETIME(ThisClass, RemainingPlayTime);
+}
+
+void AEG_GameStateBase::OnRep_RemainingCountdown()
+{
+    //UE_LOG(LogTemp, Log, TEXT("Countdown updated: %d"), RemainingCountdown);
+	EG_LOG_NET(LogEGNet, Log, TEXT("Begin: %d"), RemainingCountdown);
+}
+
+void AEG_GameStateBase::OnRep_RemainingPlayTime()
+{
+    UE_LOG(LogTemp, Log, TEXT("Play Time: %d"), RemainingPlayTime);
+    
+}
--- Source/EG/Public/GameFreamework/EG_GameStateBase.h
+++ Source/EG/Public/GameFreamework/EG_GameStateBase.h
@@ -2,9 +2,20 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+//#include "CoreMinimal.h"
 #include "GameFramework/GameStateBase.h"
 #include "EG_GameStateBase.generated.h"
+
+UENUM(BlueprintType)
+enum class EMatchState : uint8
+{
+	None,
+	Waiting,
+	Countdown,
+	Playing,
+	Ending,
+	End
+};
 
 /**
  * 
@@ -13,4 +24,28 @@ UCLASS()
 class EG_API AEG_GameStateBase : public AGameStateBase
 {
 	GENERATED_BODY()
+
+public:
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly)
+	int32 AlivePlayerControllerCount = 0;
+
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly)
+	EMatchState MatchState = EMatchState::Waiting;
+
+	UPROPERTY(ReplicatedUsing = OnRep_RemainingCountdown, VisibleAnywhere, BlueprintReadOnly)
+	int32 RemainingCountdown = 100;
+
+	 UPROPERTY(ReplicatedUsing = OnRep_RemainingPlayTime, VisibleAnywhere, BlueprintReadOnly)
+	 int32 RemainingPlayTime = 100;
+
+	UFUNCTION()
+	void OnRep_RemainingCountdown();
+
+	UFUNCTION()
+	void OnRep_RemainingPlayTime();
+
 };
