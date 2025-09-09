09b60a97be3c0d771552452976536b08b05e1b09
AmericanoTwoshot <uji06134@naver.com>
2025-09-09 13:02:22 +0900

    [Feat]: Gameplay Ability System 및 AI 종속성 추가
    게임플레이 확장과 AI 관련 기능 구현을 위해 필요했던 모듈 추가.
    - `GameplayAbilities`, `GameplayTags`, `GameplayTasks`를 추가해 GAS(Gameplay Ability System) 기반 작업 가능하도록 설정.
    - `AIModule`, `NavigationSystem` 추가로 AI 관련 로직 구현 지원.
    - 프로젝트 설정과 빌드 파일에 종속성 및 경로를 명시하여 모듈 사용 준비 완료.

--- Source/EG/EG.Build.cs
+++ Source/EG/EG.Build.cs
@@ -8,9 +8,21 @@ public class EG : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			// Initial Dependencies
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+			
+			// AI
+			"AIModule", "NavigationSystem",
+			
+			// GAS
+			"GameplayAbilities", "GameplayTags", "GameplayTasks"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PublicIncludePaths.AddRange(new string[] { "EG" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
