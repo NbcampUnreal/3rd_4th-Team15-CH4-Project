3aefd718680aaf35044f248fb7656f6307c303b8
JangJuMan <wnaks9901@gmail.com>
2025-09-09 13:47:13 +0900

    [Update] : LogJM 커스텀 로그 추가
    LogJM 카테고리
    LOG_JM
    LOG_JM_NET

--- Source/EG/EGLog.h
+++ Source/EG/EGLog.h
@@ -4,6 +4,13 @@
 
 #include "CoreMinimal.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogJM, Log, All);
+
+#define NETMODE_TCHAR ((GetNetMode() == ENetMode::NM_Client) ? *FString::Printf(TEXT("Client%02d"), UE::GetPlayInEditorID()) : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("StandAlone") : TEXT("Server")))
+#define FUNCTION_TCHAR (ANSI_TO_TCHAR(__FUNCTION__))
+#define LOG_JM(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("%s : %s"), FUNCTION_TCHAR, *FString::Printf(Format, ##__VA_ARGS__))
+#define LOG_JM_NET(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("[%s] %s %s"), NETMODE_TCHAR, FUNCTION_TCHAR, *FString::Printf(Format, ##__VA_ARGS__))
+
 /* DedicatedX 로그 예시(참고용) */
 /*
 #pragma region NetLogging
