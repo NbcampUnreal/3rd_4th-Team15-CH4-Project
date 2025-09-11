103f0b167618ccaa50d0e6a7193854fcb3567f47
JangJuMan <wnaks9901@gmail.com>
2025-09-11 12:09:23 +0900

    [Add] Chicken AnimInstance.cpp 추가

---
+++ Source/EG/Private/Character/EGChickenAnimInstance.cpp
@@ -0,0 +1,32 @@
+﻿// EGChickenAnimInstance.cpp (작성자 : JJM)
+
+#include "Character/EGChickenAnimInstance.h"
+
+#include "Character/EGChickenCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UEGChickenAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	OwnerCharacter = Cast<AEGChickenCharacter>(GetOwningActor());
+	if (IsValid(OwnerCharacter))
+	{
+		OwnerCharacterMovementComponent = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UEGChickenAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (IsValid(OwnerCharacter) == false || IsValid(OwnerCharacterMovementComponent) == false)
+	{
+		return;
+	}
+
+	Velocity = OwnerCharacterMovementComponent->Velocity;
+	GroundSpeed = FVector(Velocity.X, Velocity.Y, 0.f).Size();
+	bShouldMove = ((OwnerCharacterMovementComponent->GetCurrentAcceleration().IsNearlyZero()) == false) && (3.f < GroundSpeed);
+	bIsFalling = OwnerCharacterMovementComponent->IsFalling();
+}
