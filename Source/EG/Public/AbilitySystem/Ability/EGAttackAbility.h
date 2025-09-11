94b627cb7b51fe45fa406aed697e9a1987144a0c
sammy3831 <89569434+sammy3831@users.noreply.github.com>
2025-09-11 17:24:19 +0900

    [Feat]: 기본 능력 및 속성 시스템 클래스 추가
    새로운 캐릭터 능력 및 속성 시스템 설정을 위한 기본 클래스를 추가함.
    - 공격, 알 낳기, 쪼기 능력을 위한 GameplayAbility 클래스 생성.
    - 각 능력별 쿨다운 처리를 위한 GameplayEffect 클래스 생성.
    - EggEnergy 속성을 관리하는 AttributeSet 클래스 및 관련 리플리케이션 로직 추가.
    - 초기화 및 속성 동기화를 위한 DOREPLIFETIME 및 리플리케이션 핸들러 구현.

---
+++ Source/EG/Public/AbilitySystem/Ability/EGAttackAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "EGAttackAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EG_API UEGAttackAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
