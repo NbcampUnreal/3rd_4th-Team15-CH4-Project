016611e57373875bc536bef6fbe6e4ac8e9e27ad
sammy3831 <89569434+sammy3831@users.noreply.github.com>
2025-09-16 12:29:52 +0900

    [Add]: EggActor 생성 및 구현

---
+++ Source/EG/Public/Character/Egg/EggActor.h
@@ -0,0 +1,30 @@
+//EggActor.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EggActor.generated.h"
+
+UCLASS()
+class EG_API AEggActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEggActor();
+
+	int32 GetHealth() const;
+
+	void SetHealth(int32 NewHealth);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Root")
+	TObjectPtr<USceneComponent> RootScene;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Mesh")
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+private:
+	int32 Health = 1;
+};
