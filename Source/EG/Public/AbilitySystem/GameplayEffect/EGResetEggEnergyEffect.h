3c30ed8b2c5151eeb5fc914c78832ffb40f19f3a
sammy3831 <89569434+sammy3831@users.noreply.github.com>
2025-09-22 15:16:28 +0900

    [Feat]: 공격 능력 구현 및 관련 시스템 추가
    공격 능력을 활성화하고 사용할 수 있는 로직과 관련 시스템을 추가함.
    - UEGAttackAbility 클래스 생성 및 ActivateAbility와 EndAbility 메서드로 공격 동작 로직 구현.
    - 공격 중 알 및 캐릭터 대상 상호작용 처리 로직 추가.
    - 공격 중 적용될 몽타주 및 스턴, 에너지 초기화 효과 구현.
    - UEGStunEffect 및 UEGResetEggEnergyEffect 생성으로 상태 효과 및 에너지 초기화 처리.
    - 공격 쿨다운을 관리하는 UEGAttackCooldownEffect 구현.
    - AEGChickenCharacter에 AttackAbilityClass를 추가하고 공격 능력을 활성화하는 로직 구현.
    - FGameplayTag를 활용하여 `Ability.Cooldown.Attack` 및 `Status.Stunned` 태그 생성 및 사용.
    - Config 파일에 새로운 태그를 추가하여 쿨다운 및 상태 관리 처리.

---
+++ Source/EG/Public/AbilitySystem/GameplayEffect/EGResetEggEnergyEffect.h
@@ -0,0 +1,17 @@
+//EGResetEggEnergyEffect.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "EGResetEggEnergyEffect.generated.h"
+
+UCLASS()
+class EG_API UEGResetEggEnergyEffect : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+public:
+	UEGResetEggEnergyEffect();
+	
+};
