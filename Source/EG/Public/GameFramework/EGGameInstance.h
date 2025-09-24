7f74d6bd06c6aa8b0fc558c1cc515506236ebfdb
JangJuMan <wnaks9901@gmail.com>
2025-09-24 19:28:40 +0900

    [Add] : EG Game Instance 추가 및 기본 GI로 설정
    코드는 수정 안했고, BP_EGGameInstance 블루프린트 노드에 로그인, Voice Room Join 로직 넣어둠. (Player Controller에서 이를 호출하는 방식)
     - GI shut down(종료)시 voice room 나가기 (안하면 crash 나던데)
    
    BP Player Controller  : 1~5번 까지 매핑해둠
     1~3 : 로그인 (dev auth1, 2, persistent)
     4 : 보이스 룸 join
     5 : 보이스 룸 나가기

---
+++ Source/EG/Public/GameFramework/EGGameInstance.h
@@ -0,0 +1,16 @@
+﻿// EGGameInstance.h (작성자 : JJM)
+// Voice chat 관련 기능을 Blueprint로 작성함
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "EGGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EG_API UEGGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
