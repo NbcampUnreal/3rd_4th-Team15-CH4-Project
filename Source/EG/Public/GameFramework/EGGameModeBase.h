9a45d1debaba0aabbc24fac6bb9faedc283e172a
letterB <39991417+JangJuMan@users.noreply.github.com>
2025-09-29 15:52:30 +0900

    [PR] : PR하면서 사라진 코드 복구

--- Source/EG/Public/GameFramework/EGGameModeBase.h
+++ Source/EG/Public/GameFramework/EGGameModeBase.h
@@ -1,53 +1,46 @@
 #pragma once
-
 #include "CoreMinimal.h"
+#include "EGInGameSpawnPoints.h"
 #include "GameFramework/GameModeBase.h"
+#include "AI/EGAICharacter.h"
 #include "GameFramework/EGPlayerController.h"
 #include "EGGameModeBase.generated.h"
-
 class AEGPlayerStart;
-
-
 UCLASS()
 class EG_API AEGGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-
 public:
-
 	int32 CurrentPlayerIndex = 0;
-
 	virtual void PreLogin(const FString& Options,
 		const FString& Address,
 		const FUniqueNetIdRepl& UniqueId,
 		FString& ErrorMessage) override;
-
 	virtual void PostLogin(APlayerController* NewPlayer) override;
 	void SetRoomLeader();
 	virtual void Logout(AController* Exiting) override;
 	virtual void BeginPlay() override;
-
 	UFUNCTION(BlueprintCallable, Category="GameFlow")
 	void GameStart(int32 UniqueID);
-  
 	UFUNCTION(BlueprintCallable, Category="GameFlow")
 	void GameOver();
-
 	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;
 	void InitializeSpawnPoint();
-
 	FTimerHandle GameStartingTimerHandle;
 	FTimerHandle GameEndTimerHandle;
 	
 	UPROPERTY(EditAnywhere, Category="Spawn")
 	TSubclassOf<AEGAICharacter> AICharacter;
-
 	
 protected:
+	bool bGameStarted = false;
 	int32 LeaderNum = 0;
-
 	UPROPERTY(VisibleAnywhere, Category="Players")
 	TArray<TWeakObjectPtr<AEGPlayerController>> APlayingPlayerControllers;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spawn")
 	TMap<int32, AEGPlayerStart*> PlayerStartList;
+	UPROPERTY(VisibleAnywhere, Category="Spawn")
+	TArray<TWeakObjectPtr<AEGInGameSpawnPoints>> AInGameSpawnPoints;
+	
+	
 };
