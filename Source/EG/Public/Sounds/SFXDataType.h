fb6241e2c0126a610cf51f11a43f5a21c34c4c4f
JangJuMan <wnaks9901@gmail.com>
2025-10-02 21:56:46 +0900

    [Add] : SFX Manager Subsystem 추가
    단발성 sfx라 ugameplaystatics 로 구현.
    추후 loop나 관리가 필요하면 audiocomponent 추가해야 할 듯

---
+++ Source/EG/Public/Sounds/SFXDataType.h
@@ -0,0 +1,26 @@
+﻿// SFXDataType.h (JJM)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SFXDataType.generated.h"
+
+UENUM(BlueprintType)
+enum class ESFXType : uint8
+{
+	None		UMETA(DisplayName = "None"),
+	BlockSkill	UMETA(DisplayName = "BlockSkill"),
+		
+};
+
+USTRUCT(BlueprintType)
+struct FSFXInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	ESFXType SFXType = ESFXType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<USoundBase> SFXSound;
+};
