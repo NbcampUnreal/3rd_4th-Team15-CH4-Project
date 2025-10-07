28960042e8c308a4359c685b6bf9190c9fdf583a
JangJuMan <wnaks9901@gmail.com>
2025-10-07 20:21:05 +0900

    [Add] : 스테미나 무제한 아이템 SFX 적용
    각자 로컬 클라이언트에서만 들리도록 gameplay cue 설정 (EGGameplayCue_LocalOnly)
    
    StaminaEffect에 스택정책 설정(아이템 여러번 먹으면 지속시간 새로 갱신되도록)

---
+++ Source/EG/Private/AbilitySystem/GameplayCue/EGGameplayCue_LocalOnly.cpp
@@ -0,0 +1,76 @@
+﻿// EGGameplayCue_LocalOnly.cpp (JJM)
+
+
+#include "AbilitySystem/GameplayCue/EGGameplayCue_LocalOnly.h"
+#include "AbilitySystemGlobals.h"
+#include "EGLog.h"
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void UEGGameplayCue_LocalOnly::HandleGameplayCue(AActor* Target, EGameplayCueEvent::Type EventType,
+                                                 const FGameplayCueParameters& Parameters)
+{
+	if (!Target || !SFX)
+	{
+		EG_LOG(LogJM, Warning, TEXT("No SFX or Target"));
+		return;
+	}
+
+	if (APawn* Pawn = Cast<APawn>(Target))	// Local Client 만 SFX 재생
+	{
+		if (!Pawn->IsLocallyControlled())
+		{
+			EG_LOG(LogJM, Warning, TEXT("Target is not LocalPawn"));
+			return;
+		}
+	}
+	
+	switch (EventType)
+	{
+	case EGameplayCueEvent::OnActive:
+		EG_LOG(LogJM, Log, TEXT("On Active"));
+		PlaySFXLoopLocalOnly(Target);
+		break;
+		
+	case EGameplayCueEvent::WhileActive:
+		EG_LOG(LogJM, Log, TEXT("While Active"));
+		break;
+
+	case EGameplayCueEvent::Removed:
+		EG_LOG(LogJM, Log, TEXT("Removed"));
+		StopSFXLocalOnly();
+		break;
+
+	default:
+		break;
+	}
+}
+
+void UEGGameplayCue_LocalOnly::PlaySFXLoopLocalOnly(AActor* MyTarget)
+{
+	StopSFXLocalOnly();	// 새 아이템 먹으면 사운드 새로 재생
+	
+	// JM : 해당 SFX도 루핑 설정 해줘야 반복재생이 됨
+	AudioComponent = UGameplayStatics::SpawnSound2D(
+		GetWorld(),
+		SFX,
+		1.0f,
+		1.0f,
+		0.0f,
+		nullptr,
+		true
+	);
+}
+
+void UEGGameplayCue_LocalOnly::StopSFXLocalOnly()
+{
+	if (AudioComponent)
+	{
+		AudioComponent->Stop();
+		AudioComponent = nullptr;
+	}
+	else
+	{
+		EG_LOG(LogJM, Warning, TEXT("AudioComponent is null"));
+	}
+}
--- Source/EG/Private/Item/AbilitySystem/GameplayEffect/EGItem_StaminaBoostEffect.cpp
+++ Source/EG/Private/Item/AbilitySystem/GameplayEffect/EGItem_StaminaBoostEffect.cpp
@@ -11,6 +11,10 @@ UEGItem_StaminaBoostEffect::UEGItem_StaminaBoostEffect()
 	DurationPolicy = EGameplayEffectDurationType::HasDuration;
 	DurationMagnitude = FGameplayEffectModifierMagnitude(FScalableFloat(BoostDuration));
 
+	// JM : Staking Policy 추가 (아이템 2번 연속으로 먹으면 시간 갱신되도록)
+	StackingType = EGameplayEffectStackingType::AggregateByTarget;
+	StackDurationRefreshPolicy = EGameplayEffectStackingDurationPolicy::RefreshOnSuccessfulApplication;	
+
 	//FGameplayModifierInfo StaminaModifier;
 	//StaminaModifier.ModifierOp = EGameplayModOp::Override;
 	//StaminaModifier.ModifierMagnitude = FGameplayEffectModifierMagnitude(FScalableFloat(100.f));
@@ -22,4 +26,9 @@ UEGItem_StaminaBoostEffect::UEGItem_StaminaBoostEffect()
 	TagChanges.Added.AddTag(FGameplayTag::RequestGameplayTag(FName("Buff.StaminaBoost")));
 	TargetTags->SetAndApplyTargetTagChanges(TagChanges);
 	GEComponents.Add(TargetTags);
+
+	// JM : GameplayCue_Actor : StaminaBoost 연결 (Local Client Only 재생되도록)
+	FGameplayEffectCue StaminaBoostCue;
+	StaminaBoostCue.GameplayCueTags.AddTag(FGameplayTag::RequestGameplayTag(FName("GameplayCue.Status.StaminaBoost.LocalOnly")));
+	GameplayCues.Add(StaminaBoostCue);
 }
